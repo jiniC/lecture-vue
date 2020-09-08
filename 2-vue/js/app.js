2174b340fa1787b8f040276a61e07eee3c6b3eab
jiniC <jinic323@gmail.com>
2020-09-08 22:35:52 +0900

    검색폼

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -2,6 +2,19 @@ new Vue({
     // vue 인스턴스가 html의 어느 돔에 붙어 마운팅될지
     el: '#app',
     data: {
-        msg: 'hello word'
+        //  입력데이터 받아서 저장
+        query: ''
+    },
+    methods: {
+        onSubmit(e) {
+        },
+        onReset() {
+            // this = Vuew Instance
+            this.query = '';
+            //
+        },
+        onKeyup() {
+            if(!this.query.length) this.onReset();
+        }
     }
 })
