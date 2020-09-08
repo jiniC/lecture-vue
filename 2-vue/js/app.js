225f53f6b2fd874bc3821390725d1d865d2a4a59
jiniC <jinic323@gmail.com>
2020-09-08 22:50:29 +0900

    검색결과

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,20 +1,34 @@
+import SearchModel from './models/SearchModel.js'
 new Vue({
     // vue 인스턴스가 html의 어느 돔에 붙어 마운팅될지
     el: '#app',
     data: {
         //  입력데이터 받아서 저장
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
+            this.search();
         },
-        onReset() {
-            // this = Vuew Instance
-            this.query = '';
-            //
+        onReset(e) {
+            this.resetForm();
         },
         onKeyup() {
             if(!this.query.length) this.onReset();
-        }
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true;
+                this.searchResult = data;
+            })
+        },
+        resetForm(e) {
+            // this = Vuew Instance
+            this.query = '';
+            this.submitted = false;
+            this.searchResult = [];
+        },
     }
 })
