94f1b3a1236fa67dd15613e8d4158ef892805524
jiniC <jinic323@gmail.com>
2020-09-19 10:43:53 +0900

    ListComponent

--- 3-component/js/components/FormComponent.js
+++ 3-component/js/components/FormComponent.js
@@ -6,6 +6,13 @@ export default {
       inputValue: this.value
     }
   },
+  // ViewModel 감시하고있다가 변경되면 그 값이 변경되면 어떤 행동을 하는 함수
+  watch: {
+    // props 의 value값 감시
+    value(newVal, oldVal) {
+      this.inputValue = newVal;
+    }
+  },
   methods: {
     onSubmit() {
       this.$emit('@submit', this.inputValue.trim())
