7fa14e4e4e2324e80ab7d8f8b53a07d017ceff4d
jiniC <jinic323@gmail.com>
2020-09-19 11:33:25 +0900

    FormComponent 싱글파일컴포넌트로 추가

---
+++ 4-single-file-component/src/components/FormComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <input type="text" v-model="inputValue" v-on:keyup="onKeyup" placeholder="검색어를 입력하세요" autofocus>
+    <button v-show="inputValue.length" v-on:click="onReset" type="reset" class="btn-reset"></button>
+  </form>
+</template>
+
+<script>
+export default {
+  // template: '#search-form',
+  props: ['value'],
+  data() {
+    return {
+      inputValue: this.value
+    }
+  },
+  // ViewModel 감시하고있다가 변경되면 그 값이 변경되면 어떤 행동을 하는 함수
+  watch: {
+    // props 의 value값 감시
+    value(newVal, oldVal) {
+      this.inputValue = newVal;
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('@submit', this.inputValue.trim())
+    },
+    onKeyup() {
+      if (!this.inputValue.length) this.onReset()
+    },
+    onReset() {
+      this.inputValue = ''
+      this.$emit('@reset')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
